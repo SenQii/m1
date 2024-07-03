fa2a0c2b88cc0e87d886f43d557d4f096b86a562
SenQii <alawaji.p@gmail.com>
2024-07-03 15:14:31 +0300

    init

---
+++ src/app.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const core = require("cors");
+const app = express();
+const { Client } = require("pg");
+const { v4: uuidv4 } = require("uuid");
+require("dotenv").config();
+
+// mdlwares
+app.use(core());
+app.use(express.json());
+
+// init pg c
+const client = new Client({
+  database: process.env.DBname,
+  host: process.env.DBhost,
+  user: process.env.DBuser,
+  password: process.env.DBPass,
+  port: 44087,
+});
+
+client
+  .connect()
+  .then(() => console.log("connected to db"))
+  .catch((err) => console.log("err in connectiong to pgbd", err));
+
+// routes
+// send
+app.get("/data", async (req, res) => {
+  let data = [];
+  client.query("select * from smtc", (err, resault) => {
+    if (err) {
+      console.log("err in query", err);
+      res.status(500).send("Error executing query");
+    } else {
+      console.log("res", resault.rows);
+      data = data.push(resault.rows);
+      res.status(200).json(resault.rows);
+    }
+  });
+});
+
+// save data
+app.post("/data", async (req, res) => {
+  const { direction } = req.body;
+  console.log(req.body);
+  const id = uuidv4();
+
+  const query = `insert into smtc (id, direction) values ('${id}', '${direction}')`;
+
+  client.query(query, (err, resault) => {
+    if (err) {
+      console.log("err in query", err);
+      res.status(500).send("Error executing query");
+    } else {
+      console.log("iserted: ", direction);
+      res.status(200).json(resault.rows);
+    }
+  });
+});
+
+app.listen(3000, () => {
+  console.log(`Server is running at ${process.env.URL}`);
+});
